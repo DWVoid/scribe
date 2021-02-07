27d81000158257229d089a716952481092225195
Phillip Miao <xuhui.miao@scsstudent.org>
2021-02-07 20:35:17 +0800

    finish writer split processing

--- utils.py
+++ utils.py
@@ -7,9 +7,14 @@ import xml.etree.ElementTree as ET
 # create data file from raw xml files from iam handwriting source.
 # noinspection PyMethodMayBeStatic
 class DataParser:
-    def __init__(self, logger):
+    def __init__(self, args, logger, limit):
         self.logger = logger
         self.text_temp = {}
+        self.tsteps = args.tsteps
+        self.alphabet = args.alphabet
+        self.data_scale = args.data_scale  # scale data down by this factor
+        self.ascii_steps = int(args.tsteps / args.tsteps_per_ascii)
+        self.limit = limit # removes large noisy gaps in the data
 
     def run(self, stroke_dir, ascii_dir, writer_file, data_file):
         self.logger.write("\tparsing dataset...")
@@ -20,23 +25,43 @@ class DataParser:
         for stroke_file in self.__list_xml_files(stroke_dir):
             ascii_file = stroke_file.replace(stroke_dir, ascii_dir)[:-7] + '.txt'
             text_ascii = self.__load_ascii_file(ascii_file, int(stroke_file[-6:-4]) - 1)
-            if len(text_ascii) <= 10:
+            # check if length of the text is less than 6
+            if len(text_ascii) < self.ascii_steps:
                 self.logger.write("\tline length was too short. line was: " + text_ascii)
                 continue
+            # get the writer id
             writer_id = form_to_writer.get(ascii_file.split("/")[-1][:-4], "")
+            # check if the writer id is valid
             if writer_id == "":
                 self.logger.write("\tno writer id. line was: " + text_ascii)
                 continue
+            # get the stroke array from the xml file
+            stroke_array = self.__stroke_to_array(self.__load_stroke_file(stroke_file))
+            # check if the length of the stroke array is less than 150
+            if len(stroke_array) < self.tsteps + 1:
+                self.logger.write("\tlength of stroke points is less than 150. line was: " + text_ascii)
+                continue
+            data = self.__preprocess_data(stroke_array, text_ascii)
             # add the strokes and asciis under the specific writer
-            strokes, asciis = writer_to_data.get(writer_id, [[], []])
-            strokes.append(self.__stroke_to_array(self.__load_stroke_file(stroke_file)))
-            asciis.append(text_ascii)
-            assert len(strokes) == len(asciis), "BUG BUG"
-            writer_to_data[writer_id] = [strokes, asciis]
+            # print(writer_id)
+            # print(writer_to_data.get(writer_id, []), "\n")
+            writer_to_data[writer_id] = writer_to_data.get(writer_id, []) + [data]
 
         with open(data_file, 'wb') as f:
             pickle.dump(writer_to_data, f, protocol=2)
         self.logger.write("\tfinished parsing dataset")
+
+    # preprocess data to fit the input of the model
+    def __preprocess_data(self, stroke_array, text_ascii):
+        # removes large gaps from the data and convert to float32
+        stroke_array = np.array(np.clip(stroke_array, -self.limit, self.limit), dtype=np.float32)
+        # scale the data down
+        stroke_array[:, 0:2] /= self.data_scale
+        # put the data in the form of input
+        t_x = stroke_array[:self.tsteps]
+        t_y = stroke_array[1:self.tsteps + 1]
+        t_c = to_one_hot(text_ascii, self.ascii_steps, self.alphabet)
+        return ({'stroke': t_x, 'char': t_c}, t_y)
 
     # read a writer file and return a dictionary from form id to the writer id
     def __load_writer_file(self, writer_file):
@@ -95,14 +120,9 @@ class DataParser:
                 counter += 1
         return stroke_data
 
-
 class DataLoader:
     def __init__(self, args, logger, limit=500):
-        self.alphabet = args.alphabet
         self.batch_size = args.batch_size
-        self.tsteps = args.tsteps
-        self.data_scale = args.data_scale  # scale data down by this factor
-        self.ascii_steps = int(args.tsteps / args.tsteps_per_ascii)
         self.logger = logger
         self.limit = limit  # removes large noisy gaps in the data
 
@@ -111,79 +131,78 @@ class DataLoader:
 
         if not (os.path.exists(data_file)):
             self.logger.write("\tcreating training data cpkl file from raw source")
-            DataParser(logger).run(os.path.join(data_dir, 'lineStrokes'), os.path.join(data_dir, 'ascii'),
+            DataParser(args, logger, limit).run(os.path.join(data_dir, 'lineStrokes'), os.path.join(data_dir, 'ascii'),
                                    os.path.join(data_dir, 'forms.txt'), data_file)
 
-        self.load_preprocessed(data_file)
-        self.reset_batch_pointer()
-
-    def load_preprocessed(self, data_file):
         self.logger.write("\tloading dataset...")
         with open(data_file, 'rb') as f:
-            [raw_stroke_data, raw_ascii_data] = pickle.load(f)
+           writer_to_data = pickle.load(f)
+        
+        self.logger.write("\tassembling dataset...")
+        self.writers = list(writer_to_data.keys())
+        self.writer_to_manager = {writer_id : WriterDataManager(data, self.batch_size, logger) for writer_id, data in writer_to_data.items()}
+        self.logger.write("\t\t{} writers loaded".format(len(self.writers)))
+    
+    def validation_data(self, writer_id):
+        return self.writer_to_manager[writer_id].validation_data()
+
+    def training_data(self, writer_id, batches):
+        return self.writer_to_manager[writer_id].training_data(batches)
+
+# keep the data of a single writer
+class WriterDataManager:
+    def __init__(self, data, batch_size, logger):
+        self.batch_size = batch_size
+        self.logger = logger
+
+        self.__load_preprocessed(data)
+        self.reset_batch_pointer()
+
+    def __load_preprocessed(self, data):
         # goes thru the list, and only keeps the text entries that have more than tsteps points
         # every 1 in 20 (5%) will be used for validation data
-        self.logger.write("\tassembling dataset...")
-        self.stroke_data = []
-        self.ascii_data = []
-        self.valid_stroke_data = []
-        self.valid_ascii_data = []
+        
+        self.train_data = []
+        self.valid_data = []
         cur_data_counter = 0
-        for i, data in enumerate(raw_stroke_data):
-            if len(data) > (self.tsteps + 2):
-                # removes large gaps from the data and convert to float32
-                data = np.array(np.clip(data, -self.limit, self.limit), dtype=np.float32)
-                data[:, 0:2] /= self.data_scale
-                cur_data_counter += 1
-                if cur_data_counter % 20 == 0:
-                    self.valid_stroke_data.append(data)
-                    self.valid_ascii_data.append(raw_ascii_data[i])
-                else:
-                    self.stroke_data.append(data)
-                    self.ascii_data.append(raw_ascii_data[i])
+        for i, data in enumerate(data):
+            if cur_data_counter % 20 == 0:
+                self.valid_data.append(data)
+            else:
+                self.train_data.append(data)
+            cur_data_counter += 1
+
         # minus 1, since we want the ydata to be a shifted version of x data
-        self.num_batches = int(len(self.stroke_data) / self.batch_size)
-        self.logger.write("\t\t{} train individual data points".format(len(self.stroke_data)))
-        self.logger.write("\t\t{} valid individual data points".format(len(self.valid_stroke_data)))
-        self.logger.write("\t\t{} batches".format(self.num_batches))
+        # self.num_batches = int(len(self.stroke_data) / self.batch_size)
+        # self.logger.write("\t\t{} train individual data points".format(len(self.stroke_data)))
+        # self.logger.write("\t\t{} valid individual data points".format(len(self.valid_stroke_data)))
+        # self.logger.write("\t\t{} batches".format(self.num_batches))
 
     # returns validation data
     def validation_data(self):
-        x_batch = []
-        y_batch = []
-        ascii_list = []
+        data_batch = []
         for i in range(self.batch_size):
             index = i % len(self.valid_stroke_data)
-            data = self.valid_stroke_data[index]
-            x_batch.append(np.copy(data[:self.tsteps]))
-            y_batch.append(np.copy(data[1:self.tsteps + 1]))
-            ascii_list.append(self.valid_ascii_data[index])
-        one_hots = [to_one_hot(s, self.ascii_steps, self.alphabet) for s in ascii_list]
-        return x_batch, y_batch, one_hots
+            data_batch .append(self.valid_stroke_data[index])
+        return data_batch
 
     # returns (batches) randomized, tsteps-sized portion of the training data
     def training_data(self, batches):
-        x_batch = []
-        y_batch = []
-        ascii_list = []
+        data_batch = []
         for _ in range(batches):
             for i in range(self.batch_size):
                 index = self.idx_perm[self.pointer]
-                data = self.stroke_data[index]
-                x_batch.append(np.copy(data[:self.tsteps]))
-                y_batch.append(np.copy(data[1:self.tsteps + 1]))
-                ascii_list.append(self.ascii_data[index])
+                data_batch.append(self.train_data[index])
                 self.tick_batch_pointer()
-        one_hots = [to_one_hot(s, self.ascii_steps, self.alphabet) for s in ascii_list]
-        return x_batch, y_batch, one_hots
+        return data_batch
 
     def tick_batch_pointer(self):
         self.pointer += 1
-        if self.pointer >= len(self.stroke_data):
+        if self.pointer >= len(self.train_data):
             self.reset_batch_pointer()
 
     def reset_batch_pointer(self):
-        self.idx_perm = np.random.permutation(len(self.stroke_data))
+        self.idx_perm = np.random.permutation(len(self.train_data))
         self.pointer = 0
 
 
